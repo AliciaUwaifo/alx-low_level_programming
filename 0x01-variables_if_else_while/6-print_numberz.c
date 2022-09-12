ceab9be0bd83933443dd22a2c86be7a40c29ec96
AliciaUwaifo <aliciauwaifo@gmail.com>
2022-09-12 13:16:46 -0700

    Numberz C program

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,16 +2,18 @@
 /**
  * main - main block
  * Description: Prints numbers of base 10, starting from 0.
+ * You are not allowed to use any variable of type char.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` twice.
  * Return: 0
  */
 int main(void)
 {
-	int d = 48;
+	int i;
 
-	while (d < 58)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(d);
-		d++;
+		putchar(i + '0');
 	}
 	putchar('\n');
 
