ff3072f64ee6c0e0d86114b1e584b48087d14a39
AliciaUwaifo <aliciauwaifo@gmail.com>
2022-09-13 07:58:21 -0700

    main.h

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - main block
+ * Description: A program that prints `_putchar`, followed by a new line.
+ * Return: Always return 0
+ *
+ */
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
