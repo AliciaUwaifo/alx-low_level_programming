37c637b27acf459364e5ab45d7d12a4429a137e0
AliciaUwaifo <aliciauwaifo@gmail.com>
2022-09-13 11:38:28 -0700

    islower C program

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * _islower - Return 1 if letter is lowercase, 0 if not.
- * @c: char type letter.
- * Return 0: 1 if lowercase, 0 if not.
+ * @c: is the int that will be used in argument of the function.
+ * Return: 0
  */
 int _islower(int c)
 {
