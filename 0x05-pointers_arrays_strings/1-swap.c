3135bf43da7f49cb6a00b0adb0df9c4e3c717a30
AliciaUwaifo <aliciauwaifo@gmail.com>
2022-09-19 08:58:00 -0700

    Reverse C program

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,9 +2,9 @@
 /**
  * swap_int(int *a, int *b) - function that swaps the values
  * of two integers.
- *
  * @a: first integer
  * @b: second integer
+ * Return: always 0
  */
 void swap_int(int *a, int *b)
 {
