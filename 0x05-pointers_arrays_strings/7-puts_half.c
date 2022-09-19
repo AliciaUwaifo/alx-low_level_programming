705abc6678fc1873868620ccb1fea1d1252ce125
AliciaUwaifo <aliciauwaifo@gmail.com>
2022-09-19 13:47:08 -0700

    Winning C program

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,12 @@
 void puts_half(char *str)
 {
 	int i;
-	int oE = 0;
 
-	if (_strlen(str) % 2 != 0)
-	{
-		oE += 1;
-	}
-	for (i = (_strlen(str) + oE / 2; i < _strlen(str); i++;))
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
