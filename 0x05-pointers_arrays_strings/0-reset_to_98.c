293dc67bcaeb812554962bcae77d9c7624e77f64
AliciaUwaifo <aliciauwaifo@gmail.com>
2022-09-19 06:04:39 -0700

    98 Battery C program

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - function that takes pointer to an int as parameter.
+ * Updates the value it points to 98.
+ * @n: pointer integer
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Learning c - Pointers, arrays and strings
